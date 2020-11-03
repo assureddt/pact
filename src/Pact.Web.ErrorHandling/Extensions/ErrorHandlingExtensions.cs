4531b89e00354dbeaf77729308035dc096612ffb
welshronaldo <welshronaldo@gmail.com>
2020-11-03 23:27:31 +0000

    Lost edit somewhere

--- src/Pact.Web.ErrorHandling/Extensions/ErrorHandlingExtensions.cs
+++ src/Pact.Web.ErrorHandling/Extensions/ErrorHandlingExtensions.cs
@@ -12,7 +12,12 @@ namespace Pact.Web.ErrorHandling.Extensions
 {   
     public static class ErrorHandlingExtensions
     {
-        public static IApplicationBuilder UseTitanErrorHandling(this IApplicationBuilder builder)
+        /// <summary>
+        /// Adds opinionated Razor Pages automated error handling functionality
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UsePactErrorHandling(this IApplicationBuilder builder)
         {
             builder.UseStatusCodePagesWithReExecute("/Error/{0}");
             builder.UseExceptionHandler("/Error");
