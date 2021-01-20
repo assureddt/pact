7423939b86b1e03cfeb8b4456c25db42399259b4
welshronaldo <welshronaldo@gmail.com>
2021-01-20 17:00:27 +0000

    Add async function timer

---
+++ src/Pact.Core/Helpers/StopwatchHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Pact.Core.Helpers
+{
+    public static class StopwatchHelper
+    {
+        /// <summary>
+        /// Helper to wrap an action in a stopwatch in order to time it
+        /// </summary>
+        /// <param name="action">The action to be timed</param>
+        /// <returns>How long the action took to execute</returns>
+        public static TimeSpan Time(Action action)
+        {
+            var sw = Stopwatch.StartNew();
+
+            action();
+
+            sw.Stop();
+
+            return sw.Elapsed;
+        }
+
+        /// <summary>
+        /// Helper to wrap an asynchronous function in a stopwatch in order to time it
+        /// </summary>
+        /// <param name="func">The asynchronous function to be timed</param>
+        /// <returns>How long the function took to execute</returns>
+        public static async Task<TimeSpan> TimeAsync(Func<Task> func)
+        {
+            var sw = Stopwatch.StartNew();
+
+            await func();
+
+            sw.Stop();
+
+            return sw.Elapsed;
+        }
+    }
+}
---
+++ test/Pact.Core.Tests/StopwatchHelperTests.cs
@@ -0,0 +1,53 @@
+using System.Reflection;
+using System.Threading.Tasks;
+using Pact.Core.Extensions;
+using Pact.Core.Helpers;
+using Shouldly;
+using Xunit;
+
+namespace Pact.Core.Tests
+{
+    public class StopwatchExtensionTests
+    {
+        [Fact]
+        public void Time_Sync_AsExpected()
+        {
+            // arrange
+            var items = new[]
+            {
+                new[] {1, 2},
+                new[] {1, 2}
+            };
+
+            // act
+            var result = StopwatchHelper.Time(() =>
+            {
+                items.CartesianProduct();
+            });
+
+            // assert
+            result.TotalMilliseconds.ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task Time_Async_AsExpected()
+        {
+            // arrange
+            var file = System.IO.File.OpenRead(Assembly.GetExecutingAssembly().Location);
+
+            var buff = new byte[1024];
+            var read = 0;
+
+            // act
+            var result = await StopwatchHelper.TimeAsync(async () =>
+            {
+                read = await file.ReadAsync(buff, 0, buff.Length);
+            });
+
+            // assert
+            result.TotalMilliseconds.ShouldBeGreaterThan(0);
+            buff.ShouldContain(x => x != 0);
+            read.ShouldBe(buff.Length);
+        }
+    }
+}
