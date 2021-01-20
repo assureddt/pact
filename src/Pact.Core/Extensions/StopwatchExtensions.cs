43779ba9ea8f6890314bcf1bdfa402f0df058220
welshronaldo <welshronaldo@gmail.com>
2021-01-20 16:02:03 +0000

    Add stopwatch wrapper

---
+++ src/Pact.Core/Extensions/StopwatchExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Pact.Core.Extensions
+{
+    public static class StopwatchExtensions
+    {
+        /// <summary>
+        /// Helper to wrap an action in a stopwatch in order to time it
+        /// </summary>
+        /// <param name="action">The action to be timed</param>
+        /// <returns>How long the action took to execute</returns>
+        public static TimeSpan Time(Action action)
+        {
+            var sw = Stopwatch.StartNew();
+
+            action();
+
+            sw.Stop();
+
+            return sw.Elapsed;
+        }
+    }
+}
