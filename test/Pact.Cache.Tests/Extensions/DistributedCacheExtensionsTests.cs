2f0d67588099f3dfa1c73abe07a4366a1810f63a
Jonathan Dent <jonathan.dent@bristolitcompany.com>
2021-02-05 13:00:44 +0000

    Added AbsoluteExpirationOption extention
    Just a easy way of creating DistributedCacheEntryOptions #44

---
+++ test/Pact.Cache.Tests/Extensions/DistributedCacheExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using System;
+
+namespace Pact.Cache.Tests.Extensions
+{
+    public class DistributedCacheExtensionsTests
+    {
+        [Fact]
+        public async Task AbsoluteExpirationOption_OK()
+        {
+            // act
+            var result = Cache.Extensions.DistributedCacheExtensions.AbsoluteExpirationOption(null, TimeSpan.FromMinutes(5));
+
+            // assert
+            Assert.Null(result.SlidingExpiration);
+            Assert.True(result.AbsoluteExpirationRelativeToNow.HasValue && result.AbsoluteExpirationRelativeToNow.Value == TimeSpan.FromMinutes(5));
+        }
+    }
+}
